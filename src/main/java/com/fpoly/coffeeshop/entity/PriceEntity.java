ca9797280708e632a10759c88f5336144afbf0cd
NghiaLT <nghialtps10217@fpt.edu.vn>
2020-07-26 13:45:26 +0700

    Update PriceEntity and MenuEntity

--- src/main/java/com/fpoly/coffeeshop/entity/PriceEntity.java
+++ src/main/java/com/fpoly/coffeeshop/entity/PriceEntity.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -35,4 +37,58 @@ public class PriceEntity {
 	
 	@Column(name = "flag_delete")
 	private Boolean flagdelete;
+	
+	@ManyToOne
+	@JoinColumn(name = "menu_id")
+	private MenuEntity MenuEntity;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getStartdate() {
+		return startdate;
+	}
+
+	public void setStartdate(Date startdate) {
+		this.startdate = startdate;
+	}
+
+	public Date getEnddate() {
+		return enddate;
+	}
+
+	public void setEnddate(Date enddate) {
+		this.enddate = enddate;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Boolean getFlagdelete() {
+		return flagdelete;
+	}
+
+	public void setFlagdelete(Boolean flagdelete) {
+		this.flagdelete = flagdelete;
+	}
+
+	public MenuEntity getMenuEntity() {
+		return MenuEntity;
+	}
+
+	public void setMenuEntity(MenuEntity menuEntity) {
+		MenuEntity = menuEntity;
+	}
+	
+	
 }
