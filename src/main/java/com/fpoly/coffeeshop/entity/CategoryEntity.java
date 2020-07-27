65c7196d8fbbfb54a362320df5be5ca66f2cb730
quan1703 <quanhmps10192@fpt.edu.vn>
2020-07-27 10:06:19 +0700

    pull

--- src/main/java/com/fpoly/coffeeshop/entity/CategoryEntity.java
+++ src/main/java/com/fpoly/coffeeshop/entity/CategoryEntity.java
@@ -1,5 +1,39 @@
 package com.fpoly.coffeeshop.entity;
 
-public class CategoryEntity {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
+@Entity
+@Table(name = "categories")
+public class CategoryEntity {
+	
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "category_name")
+	private String categoryName;
+
+	@Column(name = "category_code")
+	private String categoryCode;
+
+	@Column(name = "flag_delete")
+	private Boolean flagDelete;
+
+	@OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+	@JsonBackReference
+	private List<MenuEntity> menu;
 }
