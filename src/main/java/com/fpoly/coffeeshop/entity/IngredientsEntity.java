91fbb988c4d0067fc5fa645be36970868c716a7b
quan1703 <quanhmps10192@fpt.edu.vn>
2020-08-01 01:56:38 +0700

    add IngredienttsEntity

--- src/main/java/com/fpoly/coffeeshop/entity/IngredientsEntity.java
+++ src/main/java/com/fpoly/coffeeshop/entity/IngredientsEntity.java
@@ -1,5 +1,81 @@
 package com.fpoly.coffeeshop.entity;
 
-public class IngredientsEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "ingredients")
+public class IngredientsEntity {
+	
+	
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "name")
+	private String Name;
+	
+	@Column(name= "quantity")
+	private Integer quantity;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "unit_id")
+	@JsonManagedReference
+	private UnitEntity unit;
+
+	@Column(name = "flag_delete")
+	private Boolean flagDelete;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public UnitEntity getUnit() {
+		return unit;
+	}
+
+	public void setUnit(UnitEntity unit) {
+		this.unit = unit;
+	}
+
+	public Boolean getFlagDelete() {
+		return flagDelete;
+	}
+
+	public void setFlagDelete(Boolean flagDelete) {
+		this.flagDelete = flagDelete;
+	}
+	
+	
 }
