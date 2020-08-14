b2acb2455303aedeb1883a885ccbb42fa5705690
quan1703 <58904932+quan1703@users.noreply.github.com>
2020-08-14 12:15:15 +0700

    add ICategoryRepository

---
+++ src/main/java/com/fpoly/coffeeshop/repository/ICategoryRepository.java
@@ -0,0 +1,17 @@
+package com.fpoly.coffeeshop.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fpoly.coffeeshop.entity.CategoryEntity;
+
+@Repository
+public interface ICategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+
+	
+	public CategoryEntity findOneByRoleCode(String roleCode);
+	
+	public List<CategoryEntity> findAllByFlagDeleteIs(Boolean flagDelete);
+}
