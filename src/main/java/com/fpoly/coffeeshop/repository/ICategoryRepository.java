590b3a78a02bc16de47d1658642d5429b664aef7
NghiaLT <nghialtps10217@fpt.edu.vn>
2020-08-16 01:14:11 +0700

    Add ICategoryRepository

--- src/main/java/com/fpoly/coffeeshop/repository/ICategoryRepository.java
+++ src/main/java/com/fpoly/coffeeshop/repository/ICategoryRepository.java
@@ -2,6 +2,8 @@ package com.fpoly.coffeeshop.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +12,12 @@ import com.fpoly.coffeeshop.entity.CategoryEntity;
 @Repository
 public interface ICategoryRepository extends JpaRepository<CategoryEntity, Integer> {
 
-	
-	public CategoryEntity findOneByCategoryCode(String categoryCode);
-	
 	public List<CategoryEntity> findAllByFlagDeleteIs(Boolean flagDelete);
+	
+	public Page<CategoryEntity> findAllByFlagDeleteIs(Boolean flagDelete, Pageable pageable);
+	
+	public List<CategoryEntity> findAllByCategoryCodeContaining(String categoryCode);
+	
+	public Page<CategoryEntity> findAllByCategoryCodeContaining(String categoryCode, Pageable pageable);
+	
 }
