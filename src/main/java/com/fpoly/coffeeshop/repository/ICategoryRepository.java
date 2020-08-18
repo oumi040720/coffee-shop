8a4dfb922f7efd994ff04c530af9ea8d5d8472e0
NghiaLT <nghialtps10217@fpt.edu.vn>
2020-08-18 12:43:59 +0700

    fix

--- src/main/java/com/fpoly/coffeeshop/repository/ICategoryRepository.java
+++ src/main/java/com/fpoly/coffeeshop/repository/ICategoryRepository.java
@@ -1,9 +1,7 @@
 package com.fpoly.coffeeshop.repository;
 
-import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,12 +10,5 @@ import com.fpoly.coffeeshop.entity.CategoryEntity;
 @Repository
 public interface ICategoryRepository extends JpaRepository<CategoryEntity, Integer> {
 
-	public List<CategoryEntity> findAllByFlagDeleteIs(Boolean flagDelete);
-	
-	public Page<CategoryEntity> findAllByFlagDeleteIs(Boolean flagDelete, Pageable pageable);
-	
-	public List<CategoryEntity> findAllByCategoryCodeContaining(String categoryCode);
-	
-	public Page<CategoryEntity> findAllByCategoryCodeContaining(String categoryCode, Pageable pageable);
-	
+
 }
