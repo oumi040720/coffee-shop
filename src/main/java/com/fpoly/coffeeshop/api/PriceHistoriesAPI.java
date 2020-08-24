947783ce626895ff04d6e253b911ff67e7423d0e
NghiaLT <nghialtps10217@fpt.edu.vn>
2020-08-24 13:18:01 +0700

    update PriceHistoriesAPI

--- src/main/java/com/fpoly/coffeeshop/api/PriceHistoriesAPI.java
+++ src/main/java/com/fpoly/coffeeshop/api/PriceHistoriesAPI.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +34,30 @@ public class PriceHistoriesAPI {
 		return priceHistoriesService.getTotalPages(flagDelete, page - 1, limit);
 	}
 	
+	@GetMapping(value = "/api/price_histories/flag_delete/list")
+	public List<PriceHistoriesDTO> findAllByFlagDelete(@RequestParam("flag_delete") Boolean flagDelete, @RequestParam("page") Integer page,
+														@RequestParam("limit") Integer limit){
+		return priceHistoriesService.findAllByFlagDeleteIs(flagDelete, page - 1, limit);											
+	}
 	
+	@GetMapping(value = "/api/price_histories/id/{id}")
+	public PriceHistoriesDTO findOne(@PathVariable("id") Integer id) {
+		return priceHistoriesService.findOne(id);
+	}
+	
+	@GetMapping(value = "/api/price_histories/insert")
+	public Boolean insert(@RequestBody PriceHistoriesDTO priceHistoriesDTO) {
+		return priceHistoriesService.insert(priceHistoriesDTO);
+	}
+	
+	@GetMapping(value = "/api/price_histories/update")
+	public Boolean update(@RequestBody PriceHistoriesDTO priceHistoriesDTO, @RequestParam("id") Integer id) {
+		priceHistoriesDTO.setId(id);
+		return priceHistoriesService.update(priceHistoriesDTO);
+	}
+	
+	@GetMapping(value = "/api/price_histories/delete")
+	public Boolean delete(@RequestParam("id") Integer id) {
+		return priceHistoriesService.delete(id);
+	}
 }
