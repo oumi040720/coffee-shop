91fad61b31ec7d54787a9d127101a6e62d1ce9e7
NghiaLT <nghialtps10217@fpt.edu.vn>
2020-11-02 21:51:53 +0700

    rename MenuConverter and MenuService to ProductConverter and ProductConverter

--- src/main/java/com/fpoly/coffeeshop/service/impl/ProductService.java
+++ src/main/java/com/fpoly/coffeeshop/service/impl/ProductService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import com.fpoly.coffeeshop.converter.MenuConverter;
+import com.fpoly.coffeeshop.converter.ProductConverter;
 import com.fpoly.coffeeshop.dto.ProductDTO;
 import com.fpoly.coffeeshop.entity.CategoryEntity;
 import com.fpoly.coffeeshop.entity.ProductEntity;
@@ -18,21 +18,21 @@ import com.fpoly.coffeeshop.service.IProductService;
 @Service
 public class ProductService implements IProductService {
 	@Autowired
-	private IProductRepository menuRepository;
+	private IProductRepository productRepository;
 
 	@Autowired
 	private ICategoryRepository categoryRepository;
 
 	@Autowired
-	private MenuConverter menuConverter;
+	private ProductConverter productConverter;
 
 	@Override
 	public List<ProductDTO> findAll() {
-		List<ProductEntity> list = menuRepository.findAll();
+		List<ProductEntity> list = productRepository.findAll();
 		List<ProductDTO> result = new ArrayList<>();
 
-		for (ProductEntity menu : list) {
-			result.add(menuConverter.convertToDTO(menu));
+		for (ProductEntity product : list) {
+			result.add(productConverter.convertToDTO(product));
 		}
 		return result;
 	}
@@ -40,22 +40,22 @@ public class ProductService implements IProductService {
 	@Override
 	public List<ProductDTO> findAllByCategoryCode(String categoryCode) {
 		CategoryEntity categoryEntity = categoryRepository.findOneByCategoryCode(categoryCode);
-		List<ProductEntity> list = menuRepository.findAllByCategory(categoryEntity);
+		List<ProductEntity> list = productRepository.findAllByCategory(categoryEntity);
 		List<ProductDTO> result = new ArrayList<>();
 
-		for (ProductEntity menu : list) {
-			result.add(menuConverter.convertToDTO(menu));
+		for (ProductEntity product : list) {
+			result.add(productConverter.convertToDTO(product));
 		}
 		return result;
 	}
 
 	@Override
 	public List<ProductDTO> findAllByFlagDeleteIs(Boolean flagDelete) {
-		List<ProductEntity> list = menuRepository.findAllByFlagDeleteIs(flagDelete);
+		List<ProductEntity> list = productRepository.findAllByFlagDeleteIs(flagDelete);
 		List<ProductDTO> result = new ArrayList<>();
 
-		for (ProductEntity menu : list) {
-			result.add(menuConverter.convertToDTO(menu));
+		for (ProductEntity product : list) {
+			result.add(productConverter.convertToDTO(product));
 		}
 		return result;
 	}
@@ -63,56 +63,56 @@ public class ProductService implements IProductService {
 	@Override
 	public Integer getTotalPages(Boolean flagDelete, Integer page, Integer limit) {
 
-		return menuRepository.findAllByFlagDeleteIs(flagDelete, PageRequest.of(page, limit)).getTotalPages();
+		return productRepository.findAllByFlagDeleteIs(flagDelete, PageRequest.of(page, limit)).getTotalPages();
 	}
 
 	@Override
 	public List<ProductDTO> findAllByFlagDeleteIs(Boolean flagDelete, Integer page, Integer limit) {
-		List<ProductEntity> list = menuRepository.findAllByFlagDeleteIs(flagDelete, PageRequest.of(page, limit))
+		List<ProductEntity> list = productRepository.findAllByFlagDeleteIs(flagDelete, PageRequest.of(page, limit))
 				.getContent();
 		List<ProductDTO> result = new ArrayList<>();
 
-		for (ProductEntity menu : list) {
-			result.add(menuConverter.convertToDTO(menu));
+		for (ProductEntity product : list) {
+			result.add(productConverter.convertToDTO(product));
 		}
 		return result;
 	}
 
 	@Override
 	public List<ProductDTO> findAllByProductNameContaining(String key) {
-		List<ProductEntity> list = menuRepository.findAllByProductNameContaining(key);
+		List<ProductEntity> list = productRepository.findAllByProductNameContaining(key);
 		List<ProductDTO> result = new ArrayList<>();
 
-		for (ProductEntity menu : list) {
-			result.add(menuConverter.convertToDTO(menu));
+		for (ProductEntity product : list) {
+			result.add(productConverter.convertToDTO(product));
 		}
 		return result;
 	}
 
 	@Override
 	public Integer getTotalPagesByProductNameContaining(String key, Integer page, Integer limit) {
-		return menuRepository.findAllByProductNameContaining(key, PageRequest.of(page, limit)).getTotalPages();
+		return productRepository.findAllByProductNameContaining(key, PageRequest.of(page, limit)).getTotalPages();
 	}
 
 	@Override
 	public List<ProductDTO> findAllByProductNameContaining(String key, Integer page, Integer limit) {
-		List<ProductEntity> list = menuRepository.findAllByProductNameContaining(key, PageRequest.of(page, limit))
+		List<ProductEntity> list = productRepository.findAllByProductNameContaining(key, PageRequest.of(page, limit))
 				.getContent();
 		List<ProductDTO> result = new ArrayList<>();
 
-		for (ProductEntity menu : list) {
-			result.add(menuConverter.convertToDTO(menu));
+		for (ProductEntity product : list) {
+			result.add(productConverter.convertToDTO(product));
 		}
 		return result;
 	}
 
 	@Override
 	public List<ProductDTO> findAllByFlagDeleteIsAndProductNameContaining(Boolean flagDelete, String key) {
-		List<ProductEntity> list = menuRepository.findAllByFlagDeleteIsAndProductNameContaining(flagDelete, key);
+		List<ProductEntity> list = productRepository.findAllByFlagDeleteIsAndProductNameContaining(flagDelete, key);
 		List<ProductDTO> result = new ArrayList<>();
 
-		for (ProductEntity menu : list) {
-			result.add(menuConverter.convertToDTO(menu));
+		for (ProductEntity product : list) {
+			result.add(productConverter.convertToDTO(product));
 		}
 		return result;
 	}
@@ -120,7 +120,7 @@ public class ProductService implements IProductService {
 	@Override
 	public Integer getTotalPagesByFlagDeleteIsAndProductNameContaining(Boolean flagDelete, String key, Integer page,
 			Integer limit) {
-		return menuRepository
+		return productRepository
 				.findAllByFlagDeleteIsAndProductNameContaining(flagDelete, key, PageRequest.of(page, limit))
 				.getTotalPages();
 	}
@@ -128,30 +128,30 @@ public class ProductService implements IProductService {
 	@Override
 	public List<ProductDTO> findAllByFlagDeleteIsAndProductNameContaining(Boolean flagDelete, String key, Integer page,
 			Integer limit) {
-		List<ProductEntity> list = menuRepository
+		List<ProductEntity> list = productRepository
 				.findAllByFlagDeleteIsAndProductNameContaining(flagDelete, key, PageRequest.of(page, limit))
 				.getContent();
 		List<ProductDTO> result = new ArrayList<>();
 
-		for (ProductEntity menu : list) {
-			result.add(menuConverter.convertToDTO(menu));
+		for (ProductEntity product : list) {
+			result.add(productConverter.convertToDTO(product));
 		}
 		return result;
 	}
 
 	@Override
 	public ProductDTO findOne(Integer id) {
-		return menuConverter.convertToDTO(menuRepository.getOne(id));
+		return productConverter.convertToDTO(productRepository.getOne(id));
 	}
 
 	@Override
 	public Boolean insert(ProductDTO categoryDTO) {
 		try {
 			CategoryEntity categoryEntity = categoryRepository.findOneByCategoryName(categoryDTO.getProductName());
-			ProductEntity menuEntity = menuConverter.convertToEntity(categoryDTO);
-			menuEntity.setCategory(categoryEntity);
+			ProductEntity productEntity = productConverter.convertToEntity(categoryDTO);
+			productEntity.setCategory(categoryEntity);
 
-			ProductEntity result = menuRepository.save(menuEntity);
+			ProductEntity result = productRepository.save(productEntity);
 
 			if (result != null) {
 				return true;
@@ -164,14 +164,14 @@ public class ProductService implements IProductService {
 	}
 
 	@Override
-	public Boolean update(ProductDTO menuDTO) {
+	public Boolean update(ProductDTO productDTO) {
 		try {
-			CategoryEntity categoryEntity = categoryRepository.findOneByCategoryName(menuDTO.getProductName());
-			ProductEntity oldMenu = menuRepository.getOne(menuDTO.getId());
-			ProductEntity newMenu = menuConverter.convertToEntity(menuDTO, oldMenu);
-			newMenu.setCategory(categoryEntity);
+			CategoryEntity categoryEntity = categoryRepository.findOneByCategoryName(productDTO.getProductName());
+			ProductEntity oldProduct = productRepository.getOne(productDTO.getId());
+			ProductEntity newProduct = productConverter.convertToEntity(productDTO, oldProduct);
+			newProduct.setCategory(categoryEntity);
 
-			ProductEntity result = menuRepository.save(newMenu);
+			ProductEntity result = productRepository.save(newProduct);
 			if (result != null) {
 				return true;
 			} else {
@@ -185,7 +185,7 @@ public class ProductService implements IProductService {
 	@Override
 	public Boolean delete(Integer id) {
 		try {
-			menuRepository.deleteById(id);
+			productRepository.deleteById(id);
 
 			return true;
 		} catch (Exception e) {
@@ -195,6 +195,6 @@ public class ProductService implements IProductService {
 
 	@Override
 	public ProductDTO findOne(String productName) {
-		return menuConverter.convertToDTO(menuRepository.findOneByProductName(productName));
+		return productConverter.convertToDTO(productRepository.findOneByProductName(productName));
 	}
 }
