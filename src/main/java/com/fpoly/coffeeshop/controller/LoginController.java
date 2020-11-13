02c5db6bf1403f6c57714a39ac4033cc71235419
oumi040720 <nguyenhoang04072000@gmail.com>
2020-11-13 08:09:39 +0700

    add feature register

--- src/main/java/com/fpoly/coffeeshop/controller/LoginController.java
+++ src/main/java/com/fpoly/coffeeshop/controller/LoginController.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fpoly.coffeeshop.dto.CustomersDTO;
 import com.fpoly.coffeeshop.dto.UserDTO;
+import com.fpoly.coffeeshop.service.ICustomersService;
 import com.fpoly.coffeeshop.service.IStaffService;
 import com.fpoly.coffeeshop.service.IUserService;
+import com.fpoly.coffeeshop.util.DomainUtil;
 
 @Controller
 public class LoginController {
@@ -19,7 +22,14 @@ public class LoginController {
 	
 	@Autowired
 	private IStaffService staffService;
+	
+	@Autowired
+	private ICustomersService customersService;
 
+	private String getDomain() {
+		return DomainUtil.getDoamin();
+	}
+	
 	@RequestMapping(value = "/login")
 	public String showLoginPage(HttpServletRequest request) {
 		String message = request.getParameter("message");
@@ -61,8 +71,46 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = "/registration")
-	public String showSignUpPage() {
+	public String showSignUpPage(HttpServletRequest request) {
+		request.setAttribute("domain", getDomain());
 		return "register";
+	}
+	
+	@RequestMapping(value = "/register")
+	public String register(HttpServletRequest request) {
+		String username = request.getParameter("username");
+		String email = request.getParameter("email");
+		String fullname = request.getParameter("fullname");
+		String phone = request.getParameter("phone");
+		String password = request.getParameter("password");
+		
+		UserDTO userDTO = new UserDTO();
+		userDTO.setFlagDelete(false);
+		userDTO.setPassword(password);
+		userDTO.setRoleCode("user");
+		userDTO.setUsername(username);
+		
+		CustomersDTO customersDTO = new CustomersDTO();
+		customersDTO.setAddress("");
+		customersDTO.setEmail(email);
+		customersDTO.setFlagDelete(false);
+		customersDTO.setFullname(fullname);
+		customersDTO.setPhone(phone);
+		customersDTO.setUsername(username);
+		
+		Boolean resultUser = userService.insert(userDTO);
+		
+		if (resultUser) {
+			Boolean resultCustomer = customersService.insert(customersDTO);
+			
+			if (resultCustomer) {
+				return "redirect:/login?message=message_registion_success&alert=success";
+			} else {
+				return "redirect:/login?message=message_registion_fail&alert=danger";
+			}
+		}
+		
+		return "redirect:/registration";
 	}
 
 	@RequestMapping(value = "/logout")
